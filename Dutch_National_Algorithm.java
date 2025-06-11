54d5892b895e06472cf55e7a3dd303be21f9a6ee
Abhinavu15 <152052125+Abhinavu15@users.noreply.github.com>
2025-06-11 11:11:08 +0530

    Create Dutch_National_Algorithm.java

---
+++ Dutch_National_Algorithm.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Array{
+  public static void main(String[] args){
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=s.nextInt();
+    }
+    int low=0,mid=0,high=n-1;
+    while(mid<=high){
+      switch(arr[mid]){
+        case 0:
+          int temp0=arr[low];
+          arr[low++]=arr[mid];
+          arr[mid++]=temp0;
+          break;
+        case 1:
+          mid++;
+          break;
+        case 2:
+          int temp2=arr[mid];
+          arr[mid]=arr[high];
+          arr[high--]=temp2;
+          break;
+          
+      }
+    }
+    for(int i=0;i<n;i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
